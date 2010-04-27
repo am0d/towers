77103e0a37c5d82c3d639e8177993422c677cd4f
a_m0d <damien.schoof@gmail.com>
2010-04-27 17:09:55 +0800

    Created tower type for mouse

--- tower.py
+++ tower.py
@@ -1,15 +1,21 @@
 import pygame
 import util
 
-tower_sprites = [pygame.image.load('data/towers/firetower.png')]
+def square(size, color):
+    surf = pygame.Surface(size)
+    surf.fill(color)
+    return surf
+
+tower_sprites = [square([40, 40], util.blue),
+                 square([40, 40], util.red),
+                 square([40, 40], util.green)]
 
 class Tower(pygame.sprite.Sprite):
-    def __init__(self, x, y, color=util.blue):
+    def __init__(self, x, y, image_number):
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect(x, y, 40, 40)
-        self.image = pygame.Surface([40, 40])
-        self._color = color
-        self.image.fill(self._color)
+        self.image = tower_sprites[image_number]
+        self.rect.size = self.image.get_rect().size
 
     def draw(self, screen):
         return screen.blit(self.image, self.rect)
@@ -21,14 +27,20 @@ class Tower(pygame.sprite.Sprite):
 
 class WaterTower(Tower):
     def __init__(self, x, y):
-        Tower.__init__(self, x, y)
+        Tower.__init__(self, x, y, 0)
 
 class FireTower(Tower):
     def __init__(self, x, y):
-        Tower.__init__(self, x, y, util.red)
-        self.image = tower_sprites[0]
-        self.rect.size = self.image.get_rect().size
+        Tower.__init__(self, x, y, 1)
 
 class EarthTower(Tower):
     def __init__(self, x, y):
-        Tower.__init__(self, x, y, util.green)
+        Tower.__init__(self, x, y, 2)
+
+class MouseTower(Tower):
+    def __init__(self, x, y):
+        Tower.__init__(self, x, y, 0)
+
+    def set_tower_type(self, tower_type):
+        self.image = tower_sprites[tower_type]
+        self.rect.size = self.image.get_rect().size
